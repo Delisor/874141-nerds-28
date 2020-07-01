50554227630b887e18bef49f2f640648c307e131
Delisor <lublunozhki>
2020-07-01 20:53:23 +0300

    проект почти сделан. остался сделать попап тестовый комитет для создания пул реквеста

--- js/script.js
+++ js/script.js
@@ -3,4 +3,24 @@ ymaps.ready(function () {
         center: [55.76, 37.64],
         zoom: 10
     });
+
+
+    myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+        hintContent: 'Собственный значок метки',
+        balloonContent: 'Это красивая метка'
+    }, {
+        // Опции.
+        // Необходимо указать данный тип макета.
+        iconLayout: 'default#image',
+        // Своё изображение иконки метки.
+        iconImageHref: './img/map-marker.png',
+        // Размеры метки.
+        iconImageSize: [231, 190],
+        // Смещение левого верхнего угла иконки относительно
+        // её "ножки" (точки привязки).
+        iconImageOffset: [-5, -38]
+    }),
+
+    myMap.geoObjects.add(myPlacemark)
+
 });
